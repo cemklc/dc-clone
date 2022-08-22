c67272b94b1e5e8241277c952e54d1e17fca750c
CemKilic <cem.kilic@siemens.com>
2022-08-22 14:27:57 +0300

    Add dashboard main components and add friend button.

---
+++ dc-frontend/src/Dashboard/AppBar/AppBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { styled } from '@mui/system';
+
+
+const MainContainer = styled('div')({
+    position: "absolute",
+    right: "0",
+    top: "0",
+    height: "48px",
+    backgroundColor: "#36393f",
+    width: "calc(100% - 326px)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "0 15px",
+    borderBottom: "1px solid black"
+});
+
+
+
+const AppBar = () => {
+    return (
+        <MainContainer>
+
+        </MainContainer>
+    );
+};
+
+export default AppBar;
