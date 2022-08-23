0ea57898512a1ee4a3f7857ccdcbee55ff84ac9c
CemKilic <cem.kilic@siemens.com>
2022-08-23 11:51:45 +0300

    Add friend invitation buttons

--- dc-frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
+++ dc-frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { styled } from '@mui/system';
+import PendingInvitationsListItem from './PendingInvitationsListItem';
+
+const DUMMY_INVITATIONS = [
+    {
+        _id: '1',
+        senderId: {
+            username: 'Cetka',
+            mail: 'cetka@mad.com'
+        }
+    },
+    {
+        _id: '2',
+        senderId: {
+            username: 'John',
+            mail: 'john@mad.com'
+        }
+    },
+]
+
+
 
 const MainContainer = styled('div')({
     width: '100%',
@@ -13,7 +33,14 @@ const MainContainer = styled('div')({
 const PendingInvitationsList = () => {
     return (
         <MainContainer>
-
+            {DUMMY_INVITATIONS.map((invitation) => (
+                <PendingInvitationsListItem
+                    key={invitation._id}
+                    id={invitation._id}
+                    username={invitation.senderId.username}
+                    mail={invitation.senderId.mail}
+                />
+            ))}
         </MainContainer>
     );
 };
